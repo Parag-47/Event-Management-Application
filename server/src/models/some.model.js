b84e5312e95ea8df129587e1e25294535b7d7aa7
Parag-47 <144135619+Parag-47@users.noreply.github.com>
2024-07-27 00:45:07 +0530

    first-commit

--- server/src/models/some.model.js
+++ server/src/models/some.model.js
@@ -1,2 +1,41 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
+
+const eventSchema = new Schema(
+  { 
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }
+  }
+  {
+    title: {
+      type: String,
+      require: true,
+      trim: true,
+    }
+  },
+  {
+    description: {
+      type: String,
+      trim: true,
+    }
+  },
+  {
+    location: {
+      type: String,
+      trim: true,
+    }
+  },
+  {
+    date: {
+      type: Date,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Event = mongoose.model(eventSchema);
